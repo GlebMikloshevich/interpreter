9c3d07a1229e0eabaede019c39c98c6250b2110a
GlebMikloshevich <glebmikloshevich@gmail.com>
2021-10-25 12:20:16 +0800

    added space symbol skip

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -8,62 +8,18 @@ class InterpreterException(Exception):
 class Interpreter:
 
     def __init__(self):
-        self._pos: int = 0
+        self._pos: int = -1
         self._current_token: Token = None
         self._current_char: str = None
         self._text: str = None
 
-    def _next_token(self) -> Token:
-        if self._pos >= len(self._text):
-            return Token(TokenType.EOS, None)  # ToDo fix None
-        current_char: str = self._text[self._pos]
-        if current_char.isdigit():
-            self._pos += 1
-            return Token(TokenType.INTEGER, current_char)
+    def __call__(self, text: str):
+        self.interpret(text)
 
-        if current_char == '+':
-            self._pos += 1
-            return Token(TokenType.PLUS, current_char)
-        if current_char == '-':
-            self._pos += 1
-            return Token(TokenType.MINUS, current_char)
-        raise InterpreterException(f"Bad token: {current_char}")
-
-    def _check_token_type(self, type_: TokenType):
-        if self._current_token.type_ == type_:
-            self._current_token = self._next_token()
-        else:
-            raise InterpreterException(f"Invalid token order")
-
-    def _expr(self):
-        self._current_token = self._next_token()
-        left = self._current_token
-        self._check_token_type(TokenType.INTEGER)
-        op = self._current_token
-
-        if op.type_ == TokenType.PLUS:
-            self._check_token_type(TokenType.PLUS)
-        else:
-            self._check_token_type(TokenType.MINUS)
-
-        right = self._current_token
-
-
-        self._check_token_type(TokenType.INTEGER)
-        if op.type_ == TokenType.PLUS:
-            return int(left.value) + int(right.value)
-
-        if op.type_ == TokenType.MINUS:
-            return int(left.value) - int(right.value)
-
-        raise InterpreterException("bad operation")
-
-    def __call__(self, text: str) -> int:
-        return self.interpret(str)
-
-    def interpret(self, text: str) -> int:
+    def interpret(self, text: str):
         self._text = text
-        self._pos = 0
+        self._pos = -1
+        self._forward()
         return self._expr()
 
     def _forward(self):
@@ -72,3 +28,53 @@ class Interpreter:
             self._current_char = None
         else:
             self._current_char = self._text[self._pos]
+
+    def _skip(self):
+        while self._current_char and self._current_char == ' ':
+            self._forward()
+
+    def _next_token(self) -> Token:
+        while self._current_char is not None:
+            if self._current_char == ' ':
+                self._skip()
+            if self._current_char.isdigit():
+                char = self._current_char
+                self._forward()
+                return Token(TokenType.INTEGER, char)
+
+            if self._current_char == "+":
+                char = self._current_char
+                self._forward()
+                return Token(TokenType.PLUS, char)
+
+            if self._current_char == "-":
+                char = self._current_char
+                self._forward()
+                return Token(TokenType.MINUS, char)
+
+            raise InterpreterException(f'bad token {self._current_char}')
+        return Token(TokenType.EOS, None)
+
+    def _check_token_type(self, type_: TokenType):
+        if self._current_token.type_ == type_:
+            self._current_token = self._next_token()
+        else:
+            raise InterpreterException('invalid token order')
+
+    def _expr(self) -> int:
+        self._current_token = self._next_token()
+        left = self._current_token
+        self._check_token_type(TokenType.INTEGER)
+        op = self._current_token
+        if op.type_ == TokenType.PLUS:
+            self._check_token_type(TokenType.PLUS)
+        else:
+            self._check_token_type(TokenType.MINUS)
+        right = self._current_token
+        self._check_token_type(TokenType.INTEGER)
+        if op.type_ == TokenType.PLUS:
+            return int(left.value) + int(right.value)
+        elif op.type_ == TokenType.MINUS:
+            return int(left.value) - int(right.value)
+
+        raise InterpreterException('bad operation')
